9fb011c16c98e55bf2e48e55e0f212f9896968ce
mingrutough <mingru.fe@foxmail.com>
2019-10-24 14:03:05 +0800

    router 匹配以及渲染引擎还没有做

--- test/index.js
+++ test/index.js
@@ -1,10 +1,28 @@
 const express = require('../index');
 const app = express();
+const router = express.Router();
+console.log(typeof express.Router());
+app.use(function test1(req, res, next) {
+    console.log('Time：', Date.now());
+    next();
+});
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function test2(req, res, next) {
+    res.send('first');
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
+router.use(function test3(req, res, next) {
+    console.log('Time2: ', Date.now());
+    next();
+});
+
+router.use('/', function test4(req, res, next) {
+    res.send('second');
+});
+
+app.use('/user', router);
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
